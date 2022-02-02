291cbd32d5e095b694a67ada608fdc208f6f222b
Renanppcosta <renanppcosta@gmail.com>
2022-02-02 11:33:11 -0300

    Adicionando novos Arquivos.js

---
+++ alterar.js
@@ -0,0 +1,7 @@
+//const fs = require("fs")
+
+//const posicao = process.argv[2]
+
+//const pessoas = require("./pessoas.json")
+
+//pessoas.splice(posicao,1)
---
+++ mostrarCasados.js
@@ -0,0 +1,7 @@
+const pessoas = require("./pessoas.json")
+
+
+
+let casados = pessoas.filter(p => p.casado == true)
+
+console.table(casados)
